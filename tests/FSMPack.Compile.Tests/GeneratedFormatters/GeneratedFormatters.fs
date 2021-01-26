180b2f97afc0636e92a4fe554ec2a780a267b5e5
Guo Sun <gsuuon@gmail.com>
2021-01-26 18:30:36 -0500

    Reference types proj, simple formatters for records

--- tests/FSMPack.Compile.Tests/GeneratedFormatters/GeneratedFormatters.fs
+++ tests/FSMPack.Compile.Tests/GeneratedFormatters/GeneratedFormatters.fs
@@ -42,3 +42,51 @@ type FormatMyInnerType() =
             {
                 C = C
             }
+
+
+type FormatMyTestType() =
+    interface Format<MyTestType> with
+        member _.Write bw (v: MyTestType) =
+            writeMapFormat bw 3
+            writeValue bw (RawString "A")
+            writeValue bw (Integer v.A)
+            writeValue bw (RawString "B")
+            writeValue bw (FloatDouble v.B)
+            writeValue bw (RawString "inner")
+            Cache<MyInnerType>.Retrieve().Write bw v.inner
+
+        member _.Read (br, bytes) =
+            let count = 3
+            let expectedCount = readMapFormatCount br &bytes
+
+            if count <> expectedCount then
+                failwith
+                    ("Map has wrong count, expected " + string count
+                        + " got " + string expectedCount)
+
+            let mutable items = 0
+            let mutable A = Unchecked.defaultof<Int32>
+            let mutable B = Unchecked.defaultof<Double>
+            let mutable inner = Unchecked.defaultof<MyInnerType>
+            while items < count do
+                match readValue br &bytes with
+                | RawString key ->
+                    match key with
+                    | "A" ->
+                        let (Integer x) = readValue br &bytes
+                        A <- x
+                    | _ -> failwith "Unknown key"
+                    | "B" ->
+                        let (FloatDouble x) = readValue br &bytes
+                        B <- x
+                    | _ -> failwith "Unknown key"
+                    | "inner" ->
+                        inner <- Cache<MyInnerType>.Retrieve().Read(br, bytes)
+                    | _ -> failwith "Unknown key"
+                items <- items + 1
+
+            {
+                A = A
+                B = B
+                inner = inner
+            }
