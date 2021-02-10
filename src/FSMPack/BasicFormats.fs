46ad3204e27434c4b2eb15a5dff16ce131129d7b
Guo Sun <gsuuon@gmail.com>
2021-02-10 13:15:03 -0500

    Factor out unecessary inheritance

--- src/FSMPack/BasicFormats.fs
+++ src/FSMPack/BasicFormats.fs
@@ -9,67 +9,61 @@ open FSMPack.Format
 
 #nowarn "0025"
 
+let writeIDict (bw, v: IDictionary<'K, 'V>) =
+    let keyFormat = Cache<'K>.Retrieve()
+    let valueFormat = Cache<'V>.Retrieve()
+
+    writeMapFormat bw v.Count
+
+    Seq.iter
+        (fun (kv: KeyValuePair<'K, 'V>) ->
+            keyFormat.Write bw kv.Key
+            valueFormat.Write bw kv.Value )
+        v
+
+let readIDict (br, bytes) =
+    let keyFormat = Cache<'K>.Retrieve()
+    let valueFormat = Cache<'V>.Retrieve()
+
+    let expectedCount = readMapFormatCount br &bytes
+
+    let mutable items = 0
+    let collection = Dictionary()
+
+    while items < expectedCount do
+        let key = keyFormat.Read (br, bytes)
+        let value = valueFormat.Read (br, bytes)
+        collection.[key] <- value
+        items <- items + 1
+
+    collection :> IDictionary<'K, 'V>
+
+// TODO if 'K and 'V are msgpack types, we could use Value.MapCollection
 type FormatIDictionary<'K, 'V when 'K : comparison>() =
-    member _.WriteBase (bw, v: IDictionary<'K, 'V>) =
-        let keyFormat = Cache<'K>.Retrieve()
-        let valueFormat = Cache<'V>.Retrieve()
-
-        writeMapFormat bw v.Count
-
-        Seq.iter
-            (fun (kv: KeyValuePair<'K, 'V>) ->
-                keyFormat.Write bw kv.Key
-                valueFormat.Write bw kv.Value )
-            v
-
-    member _.ReadBase (br, bytes) =
-        let keyFormat = Cache<'K>.Retrieve()
-        let valueFormat = Cache<'V>.Retrieve()
-
-        let expectedCount = readMapFormatCount br &bytes
-
-        let mutable items = 0
-        let collection = Dictionary()
-
-        while items < expectedCount do
-            let key = keyFormat.Read (br, bytes)
-            let value = valueFormat.Read (br, bytes)
-            collection.[key] <- value
-            items <- items + 1
-
-        collection :> IDictionary<'K, 'V>
-
     interface FSMPack.Format.Format<IDictionary<'K, 'V>> with
-    // TODO if 'K and 'V are msgpack types, we could use Value.MapCollection
-        member this.Write bw (v: IDictionary<'K, 'V>) =
-            this.WriteBase (bw, v)
-
-        member this.Read (br, bytes) =
-            this.ReadBase (br, bytes)
-
-type FormatMap<'K, 'V when 'K : comparison>() =
-    inherit FormatIDictionary<'K, 'V>()
-    interface Format<Map<'K, 'V>> with
-        // FIXME FS0419 can't use curried members of base class
-        // Need to change interface Format.Write to (bw, v)
-        // but I think there were issues with that due to byref
-        // Generally should avoid curried methods for C# interop
-        member _.Write bw (v: Map<'K, 'V>) =
-            base.WriteBase (bw, v)
+        member _.Write bw (v: IDictionary<'K, 'V>) =
+            writeIDict (bw, v)
 
         member _.Read (br, bytes) =
-            base.ReadBase (br, bytes)
+            readIDict (br, bytes)
+
+type FormatMap<'K, 'V when 'K : comparison>() =
+    interface Format<Map<'K, 'V>> with
+        member _.Write bw (v: Map<'K, 'V>) =
+            writeIDict (bw, v)
+
+        member _.Read (br, bytes) =
+            readIDict (br, bytes)
             |> Seq.map (|KeyValue|)
             |> Map.ofSeq
 
 type FormatDictionary<'K, 'V when 'K : comparison>() =
-    inherit FormatIDictionary<'K, 'V>()
     interface Format<Dictionary<'K, 'V>> with
         member _.Write bw v =
-            base.WriteBase (bw, v)
+            writeIDict (bw, v)
 
         member _.Read (br, bytes) =
-            base.ReadBase (br, bytes)
+            readIDict (br, bytes)
             |> Dictionary
             
 
